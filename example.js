1d7739083edbe45061a6672b5906c081f979df48
Brent Jackson <jacksonblack@gmail.com>
2015-03-09 21:46:23 -0400

    Rename theme prop

--- example.js
+++ example.js
@@ -4,15 +4,19 @@ var highlight = require('highlight.js');
 var classnames = require('classnames');
 var reactToJsx = require('react-to-jsx');
 
+var JS_COMMENT_REGEX = /(\/\*[\w\'\s\r\n\*]*\*\/)|(\/\/[\w\s\']*)|(\<![\-\-\s\w\>\/]*\>)/;
+
 module.exports = React.createClass({displayName: "exports",
 
   getDefaultProps: function() {
     return {
-      theme: 'silver'
+      color: 'silver'
     }
   },
 
   highlight: function(string) {
+    // Attempting to remove js comments
+    //var string = string.replace(JS_COMMENT_REGEX, '');
     var code = highlight.highlight('xml', string).value;
     var html = { __html: code };
     return React.createElement("pre", {dangerouslySetInnerHTML: html, className: "p2 mb0 rounded-bottom bg-darken-1"})
@@ -22,7 +26,7 @@ module.exports = React.createClass({displayName: "exports",
 
     var string = reactToJsx(this.props.children, { indent: '  ' });
     var classes = {
-      container: classnames('mb2', 'border', 'rounded', 'border-' + this.props.theme),
+      container: classnames('mb2', 'border', 'rounded', 'border-' + this.props.color),
       rendered: classnames('p2'),
     };
 
